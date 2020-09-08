e87304be890cdee16b8c8592b8fd3f4533f49590
Abhishek Kumar Pandey <abhishekpandey.kr@gmail.com>
2020-09-08 10:47:42 +0530

    index.html for routing

--- src/root-application/index.js
+++ src/root-application/index.js
@@ -4,18 +4,23 @@ import * as singleSpa from "single-spa";
 singleSpa.registerApplication(
   "App1",
   () => import("../react1/app1.js"),
-  pathPrefix("/app1")
+  (location) =>
+    location.pathname === "" ||
+    location.pathname === "/" ||
+    location.pathname.startsWith("/app1")
 );
 singleSpa.registerApplication(
   "App2",
   () => import("../react2/app2.js"),
-  pathPrefix("/app2")
+  (location) => location.pathname.startsWith("/app2")
+  // pathPrefix("/app2")
 );
 
 singleSpa.registerApplication(
   "App3",
   () => import("../react3/app3.js"),
-  pathPrefix("/app3")
+  (location) => location.pathname.startsWith("/app3")
+  // pathPrefix("/app3")
 );
 
 singleSpa.start();
